f96c9607d3198ed4e38b0a24a04dc316e0c8b5a9
momin_thabet <momin.thabet@gmail.com>
2022-12-20 15:14:21 +0200

    update test to imply for db changes

---
+++ server/src/services/mongo.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connection.once('open' ,()=>{
+    console.log('MongoDB connection ready!!');
+} );
+
+mongoose.connection.on('error', (err)=>{
+    console.error(err);
+})
+
+
+async function mongoConnect(){
+    await mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser : true,  
+        useUnifiedTopology : true
+    });
+}
+
+async function mongoDisconnect(){
+    await mongoose.disconnect();
+}
+
+
+module.exports = {
+    mongoConnect,
+    mongoDisconnect
+    
+}
