012f62d9b283903cdd80a5f85b3ec5427f75cba2
momin_thabet <momin.thabet@gmail.com>
2022-12-21 19:47:55 +0200

    working with spaceX api

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,85 +1,146 @@
+const axios = require('axios');
+
 const launchesDataBase = require('./launches.mongo');
 const planets = require('./planets.mongo');
 
 const DEFAULT_FLIGHT_NUMBER = 100;
 
-const launch = {
-    flightNumber : 100,
-    mission :'Kepler Exploration X',
-    rocket:'Explorer IS1',
-    launchDate :new Date('December 27, 2030'),
-    target : 'Kepler-442 b',
-    customers : ['ZTM','NASA'],
-    upcoming : true,
-    success : true,
-};
+async function populateLaunches() {
+    console.log('loading');
+    const response = await axios.post(process.env.SPACEX_API_URL, {
+        query: {},
+        options: {
+            pagination: false,
+            populate: [
+                {
+                    path: 'rocket',
+                    select: {
+                        name: 1,
+                    },
+                },
+                {
+                    path: 'payloads',
+                    select: {
+                        customers: 1,
+                    },
+                },
+            ],
+        },
+    });
 
-// launches.set(launch.flightNumber ,launch);
-saveLaunch(launch);
+    if (response.status !== 200) {
+        console.log('Problem downloading launch data');
+        throw new Error('Launch data download failed');
+    }
+    const launchDocs = response.data.docs;
+    for (const launchDoc of launchDocs) {
+        const payloads = launchDoc['payloads'];
+        const customers = payloads.flatMap((payload) => {
+            return payload['customers'];
+        });
 
-async function existsLaunchWithId(launchId){
-    return await launchesDataBase.findOne({
-        flightNumber : launchId
-    }) ; 
+        const launch = {
+            flightNumber: launchDoc['flight_number'],
+            mission: launchDoc['name'],
+            rocket: launchDoc['rocket']['name'],
+            launchDate: launchDoc['date_local'],
+            upcoming: launchDoc['upcoming'],
+            success: launchDoc['success'],
+            customers,
+        };
+        console.log(`${launch.flightNumber} ${launch.mission}`);
+
+        //populate launches collection ...
+        await saveLaunch(launch);
+    }
 }
 
-async function getLatestFlightNumber(){
-    const latestLaunch = await launchesDataBase
-        .findOne()
-        .sort('-flightNumber');
-    if(!latestLaunch) return DEFAULT_FLIGHT_NUMBER ;
+async function loadLaunchData() {
+    const firstLaunch = await findLaunch({
+        flightNumber: 1,
+        rocket: 'Falcon 1',
+        mission: 'FalconSat',
+    });
+    if (firstLaunch) {
+        console.log('Launch data already loaded');
+    } else {
+        populateLaunches();
+    }
+}
+
+async function findLaunch(filter) {
+    return await launchesDataBase.findOne(filter);
+}
+
+async function existsLaunchWithId(launchId) {
+    return await findLaunch({
+        flightNumber: launchId,
+    });
+}
+
+async function getLatestFlightNumber() {
+    const latestLaunch = await launchesDataBase.findOne().sort('-flightNumber');
+    if (!latestLaunch) return DEFAULT_FLIGHT_NUMBER;
     return latestLaunch.flightNumber;
 }
 
-async function getAllLaunches(){
-        return await launchesDataBase.find({},{
-            '_id':0,
-            '__v':0
-        })
-};
-
-async function saveLaunch(launch){
-    const planet = await planets.findOne({
-        keplerName : launch.target,
-    });
-    if (!planet){
-        throw new Error('No matching planet found');
-    };
-
-    await launchesDataBase.findOneAndUpdate({
-        flightNumber: launch.flightNumber
-    },launch,{
-        upsert :true ,
-    })
+async function getAllLaunches(skip, limit) {
+    return await launchesDataBase
+        .find({}, { _id: 0, __v: 0 })
+        .sort('flightNumber')
+        .skip(skip)
+        .limit(limit);
 }
 
-async function scheduleNewLaunch(launch){
-    const newFlightNumber = await getLatestFlightNumber() +1;
-    const newLaunch = Object.assign(launch ,{
-        success :true ,
-        upcoming :true,
-        customers :['ZTM','NASA'],
-        flightNumber :newFlightNumber
+async function saveLaunch(launch) {
+    await launchesDataBase.findOneAndUpdate(
+        {
+            flightNumber: launch.flightNumber,
+        },
+        launch,
+        {
+            upsert: true,
+        }
+    );
+}
+
+async function scheduleNewLaunch(launch) {
+    const planet = await planets.findOne({
+        keplerName: launch.target,
+    });
+
+    if (!planet) {
+        throw new Error('No matching planet found');
+    }
+
+    const newFlightNumber = (await getLatestFlightNumber()) + 1;
+    const newLaunch = Object.assign(launch, {
+        success: true,
+        upcoming: true,
+        customers: ['ZTM', 'NASA'],
+        flightNumber: newFlightNumber,
     });
     await saveLaunch(newLaunch);
-
 }
 
-async function abortLaunchById(launchId){
-    const aborted = await launchesDataBase.updateOne({
-        flightNumber : launchId
-    },{
-        upcoming :false,
-        success:false
-    });
+async function abortLaunchById(launchId) {
+    const aborted = await launchesDataBase.updateOne(
+        {
+            flightNumber: launchId,
+        },
+        {
+            upcoming: false,
+            success: false,
+        }
+    );
 
-    return aborted.acknowledged ;
+    return aborted.acknowledged;
 }
 
-
-module.exports ={
+module.exports = {
     existsLaunchWithId,
     getAllLaunches,
     scheduleNewLaunch,
-    abortLaunchById
+    abortLaunchById,
+    loadLaunchData,
 };
--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
-mongoose.connection.once('open' ,()=>{
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once('open', () => {
     console.log('MongoDB connection ready!!');
-} );
+});
 
-mongoose.connection.on('error', (err)=>{
+mongoose.connection.on('error', (err) => {
     console.error(err);
-})
+});
 
-
-async function mongoConnect(){
-    await mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser : true,  
-        useUnifiedTopology : true
+async function mongoConnect() {
+    await mongoose.connect(MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
     });
 }
 
-async function mongoDisconnect(){
+async function mongoDisconnect() {
     await mongoose.disconnect();
 }
 
-
 module.exports = {
     mongoConnect,
-    mongoDisconnect
-    
-}
+    mongoDisconnect,
+};
